8d5638e801537f303c5effc5b24bee459ec06c9d
Malena Gianoglio <malenagianoglio@gmail.com>
2025-09-08 19:03:54 -0300

    Arreglos.

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,3 +1,7 @@
+import { createContext, useState, useEffect } from "react";
+
+export const AuthContext = createContext();
+
 export function AuthProvider({ children }) {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [user, setUser] = useState(null); 
